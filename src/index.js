6410d435bf2b057a69a60524f350f30a0b1c7dac
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-07-12 20:00:00 -0300

    Remove file unnecessary

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const HealhSecretaryRepository = require("./Repository/HealthSecretaryRepository");
-
-new HealhSecretaryRepository()
-    .getStatusCovid()
-    .then(console.log)
-    .catch(console.log)
